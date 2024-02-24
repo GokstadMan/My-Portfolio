6b524e2daab7bdd56366beba816d57ea22bb3d42
Benjo <ben.ohman@gokstadakademiet.no>
2024-02-24 07:54:51 +0100

    first version my portfolio homepage

---
+++ src/components/Experience.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+import html from "../images/html.png";
+import css from "../images/css.png";
+import javascript from "../images/javascript.png";
+import reactImage from "../images/react.png";
+import nextjs from "../images/nextjs.png";
+import bulma from "../images/bulma.png";
+import bootstrap from"../images/bootstrap.png";
+import express from "../images/express.png";
+import python from "../images/python.png";
+import sql from "../images/sql.png";
+import github from "../images/github.png";
+import tailwind from "../images/tailwind.png";
+
+
+
+const Experience = () => {
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 6,
+      src: bulma,
+      title: "BULMA",
+      style: "shadow-[#00caac]",
+    },
+    {
+      id: 7,
+      src: bootstrap,
+      title: "Bootstrap",
+      style: "shadow-[#8663BA]",
+    },
+    {
+      id: 8,
+      src: nextjs,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 9,
+      src: express,
+      title: "Express",
+      style: "shadow-[#9BCB52]",
+      },
+    {
+      id: 10,
+      src: python,
+      title: "Python",
+      style: "shadow-[#FACC50]",
+      },
+    {
+      id: 11,
+      src: sql,
+      title: "Sql",
+      style: "shadow-[#3B84CC]",
+      },
+    {
+      id: 12,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+  ];
+
+  return (
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-gray-900 w-full h-auto"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Experience
+          </p>
+          <p className="py-6">These are the technologies I've worked with:</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {techs.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
